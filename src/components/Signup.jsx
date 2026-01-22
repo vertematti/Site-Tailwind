f49fc352981cd0de7d3d0223851f90c15bd74fe4
Gerson Luis Vertematti <gersonlv@gmail.com>
2026-01-22 18:37:48 -0300

    Adição do SignUp do Supabase

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { UserAuth } from '../context/AuthContext';
 
 const Signup = () => {
@@ -9,11 +9,29 @@ const Signup = () => {
   const [error, setError] = useState("");
   const [loading, setLoading] = useState("");
 
-  const { session } = UserAuth();
+  const { session, signUpNewUser } = UserAuth();
+  const navigate = useNavigate();
+
+  console.log(session);
+
+  const handleSignUp = async (e) => {
+    e.preventDefault()
+    setLoading(true);
+    try {
+      const result = await signUpNewUser(email, password)
+      if (result.success) {
+        Navigate("./dashboard");
+      }
+    } catch (err) {
+      setError("Ocorreu um erro no cadastro: ", err);
+    } finally {
+      setLoading(false);
+    }
+  }
 
   return (
     <div>
-      <form className='max-w-md m-auto pt-4'>
+      <form onSubmit={handleSignUp} className='max-w-md m-auto pt-4'>
         <h2 className='font-bold pb-2'>
           Cadastre-se hoje!
         </h2>
@@ -21,9 +39,10 @@ const Signup = () => {
           Já possue uma conta? <Link to='./signin'>Entrar!</Link>
         </p>
         <div className='flex flex-col py-4'>
-          <input placeholder='E-mail' className='p-2 rounded border border-gray-300 mt-6 bg-gray-700' type="email" />
-          <input placeholder='Password' className='p-2 rounded border border-gray-300 mt-6 bg-gray-700' type="password" />
+          <input onChange={(e) => setEmail(e.target.value)} placeholder='E-mail' className='p-2 rounded border border-gray-300 mt-6 bg-gray-700' type="email" />
+          <input onChange={(e) => setPassword(e.target.value)} placeholder='Password' className='p-2 rounded border border-gray-300 mt-6 bg-gray-700' type="password" />
           <button disabled={loading} className='mt-6 w-full' type='submit'>Cadastrar</button>
+          {error && <p className='text-red-600 text-center pt-4'>{error}</p>}
         </div>
       </form>
     </div>
--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,4 +1,5 @@
 import { createContext, useEffect,useState, useContext } from "react";
+import { supabase } from "../supabaseClient";
 
 const AuthContext = createContext()
 
@@ -6,9 +7,56 @@ export const AuthContextProvider = ({ children }) => {
 
     const [ session, setSession ] = useState(undefined)
 
+    const signUpNewUser = async (email, password) => {
+        const {data, error} = await supabase.auth.signUp({
+            email: email,
+            password: password,
+        });
+        if(error) {
+            console.error("Houve um erro de autenticação: ", error);
+            return { success: false, error: error.message };
+        }
+        return { success: true, data};
+    };
+
+    const signInUser = async ({ email, password }) => {
+        try {
+            const {data, error} = await supabase.auth.signInWithPassword({
+                email: email,
+                password: password,
+            }); 
+            if(error) {
+                console.error("Houve um erro de autenticação: ", error);
+                return { success: false, error: error.message };
+            }
+            console.log("Login efetuado com sucesso.", data);
+            return { success: true, data};
+        } catch(error) {
+            console.error("Houve um erro no Login: ", error);
+        }
+    };
+
+    useEffect(() => {
+
+        supabase.auth.getSession().then(({ data: {session} }) => {
+            setSession(session);
+        });
+
+        supabase.auth.onAuthStateChange((_event, session) => {
+            setSession(session);
+        });
+    }, []);
+
+    const signOut = () => {
+        const { error } = supabase.auth.signOut();
+        if (error) {
+            console.error("Houve um problema no SignOut: ", error);
+        }
+    };
+
     return (
 
-        <AuthContext.Provider value={{session}}>
+        <AuthContext.Provider value={{ session, signUpNewUser, signInUser, signOut }}>
             {children}
         </AuthContext.Provider>
 
