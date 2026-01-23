48f9a2adeec28d28254a70bf1a19a3407ea19704
Gerson Luis Vertematti <gersonlv@gmail.com>
2026-01-23 00:32:40 -0300

    Implementação do SignOut

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import { UserAuth } from '../context/AuthContext'
+import { useNavigate } from 'react-router-dom';
 
 const Dashboard = () => {
+
+  const { session, signOut } = UserAuth();
+  const navigate = useNavigate();
+
+  console.log(session);
+
+  const handleSignOut = async (e) => {
+    e.preventDefault();
+    try {
+      await signOut();
+      navigate("/");
+    } catch(err) {
+      console.error(err);
+    }
+  };
+
   return (
-    <div>Dashboard</div>
+    <div>
+      <h1>Dashboard</h1>
+      <h2>Bem vindo {session?.user?.email}</h2>
+      <div>
+        <p onClick={handleSignOut} 
+        className='hover:cursor-pointer border inline-block px-4 py-3 mt-4'>Sair</p>
+      </div>
+    </div>
   )
 }
 
